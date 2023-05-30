a0aad06baf0296aacc9f49ce6ab9aa0afc3d7d25
HananINouman <hanan@obol.tech>
2023-05-30 22:36:03 +0300

    pass signer instead of signature

--- packages/obol-sdk/src/index.ts
+++ packages/obol-sdk/src/index.ts
@@ -1,17 +1,26 @@
 
+import { ethers } from 'ethers';
 import { Base } from './base';
 import { CONFLICT_ERROR_MSG } from './constants';
 import { ConflictError } from './errors';
 import { Cluster } from './types';
 
-
 export class Client extends Base {
+
+  private signer: ethers.Wallet;
+
+  constructor(config: any, signer: ethers.Wallet) {
+    super(config)
+    this.signer = signer
+  }
 
   /**
    * @param cluster The new unique cluster
    * @returns The saved cluster from DB
   */
-  createCluster(newCluster: Cluster, creatorConfigSignature: string): Promise<unknown>{
+  async createCluster(newCluster: Cluster, signer: any): Promise<unknown> {
+    const data = getCreatorConfigHashMessage({ creator_config_hash: newCluster.config_hash }, 5);
+    const creatorConfigSignature = await signer.signTypedData(data.domain, data.types, data.message)
     return this.request(`/dv`, {
       method: 'POST',
       body: JSON.stringify(newCluster),
@@ -48,3 +57,25 @@ export class Client extends Base {
     });
   }
 }
+
+export const EIP712_DOMAIN_NAME = "Obol";
+export const EIP712_DOMAIN_VERSION = "1";
+export const CreatorConfigHashSigningTypes = {
+  CreatorConfigHash: [{ name: "creator_config_hash", type: "string" }],
+};
+
+const getCreatorConfigHashMessage = (
+  payload: any,
+  chainId: any,
+): any => ({
+  types: CreatorConfigHashSigningTypes,
+  primaryType: "CreatorConfigHash",
+  domain: {
+    name: EIP712_DOMAIN_NAME,
+    version: EIP712_DOMAIN_VERSION,
+    chainId,
+  },
+  message: payload,
+});
+
+
