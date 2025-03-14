2c71a1d4f1043f8d86e72b91204a785dd6c5b35b
HananINouman <hanan@obol.tech>
2025-03-14 20:02:30 +0300

    fetch incentives by network

--- test/incentives.test.ts
+++ test/incentives.test.ts
@@ -1,5 +1,5 @@
 import { ethers, JsonRpcProvider } from 'ethers';
-import { Client } from '../src/index';
+import { Client, FORK_NAMES } from '../src/index';
 import * as utils from '../src/utils';
 import * as incentivesHelpers from '../src/incentivesHalpers';
 import { DEFAULT_BASE_VERSION } from '../src/constants';
@@ -213,7 +213,7 @@ describe('Client.incentives', () => {
     expect(result).toEqual(mockIncentives);
 
     expect(global.fetch).toHaveBeenCalledWith(
-      `${baseUrl}/${DEFAULT_BASE_VERSION}/address/incentives/${mockAddress}`,
+      `${baseUrl}/${DEFAULT_BASE_VERSION}/address/incentives/holesky/${mockAddress}`,
       expect.objectContaining({ method: 'GET' }),
     );
   });
