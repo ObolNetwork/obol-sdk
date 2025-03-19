5fd6b5c0256ef982805e55b6fcda859ec917fd95
HananINouman <hanan@obol.tech>
2025-03-19 18:41:52 +0300

    format

--- src/incentives.ts
+++ src/incentives.ts
@@ -1,26 +1,52 @@
-import type { JsonRpcApiProvider, JsonRpcProvider, JsonRpcSigner, Provider, Signer } from 'ethers'
-import { isContractAvailable } from './utils'
-import { type Incentives as IncentivesType, type ETH_ADDRESS, FORK_NAMES } from './types'
-import { claimIncentivesFromMerkleDistributor, isClaimedFromMerkleDistributor } from './incentiveHelpers'
-import { DEFAULT_BASE_VERSION } from './constants'
+import type {
+  JsonRpcApiProvider,
+  JsonRpcProvider,
+  JsonRpcSigner,
+  Provider,
+  Signer,
+} from 'ethers';
+import { isContractAvailable } from './utils';
+import {
+  type Incentives as IncentivesType,
+  type ETH_ADDRESS,
+  FORK_NAMES,
+} from './types';
+import {
+  claimIncentivesFromMerkleDistributor,
+  isClaimedFromMerkleDistributor,
+} from './incentiveHelpers';
+import { DEFAULT_BASE_VERSION } from './constants';
 
 export class Incentives {
-  private readonly signer: Signer | JsonRpcSigner | undefined
-  public readonly chainId: number
-  private readonly request: (endpoint: string, options?: RequestInit) => Promise<any>
+  private readonly signer: Signer | JsonRpcSigner | undefined;
+  public readonly chainId: number;
+  private readonly request: (
+    endpoint: string,
+    options?: RequestInit,
+  ) => Promise<any>;
 
-  public readonly provider: Provider | JsonRpcProvider | JsonRpcApiProvider | undefined | null
+  public readonly provider:
+    | Provider
+    | JsonRpcProvider
+    | JsonRpcApiProvider
+    | undefined
+    | null;
 
   constructor(
     signer: Signer | JsonRpcSigner | undefined,
     chainId: number,
     request: (endpoint: string, options?: RequestInit) => Promise<any>,
-    provider: Provider | JsonRpcProvider | JsonRpcApiProvider | undefined | null,
+    provider:
+      | Provider
+      | JsonRpcProvider
+      | JsonRpcApiProvider
+      | undefined
+      | null,
   ) {
-    this.signer = signer
-    this.chainId = chainId
-    this.request = request
-    this.provider = provider
+    this.signer = signer;
+    this.chainId = chainId;
+    this.request = request;
+    this.provider = provider;
   }
 
   /**
@@ -35,31 +61,38 @@ export class Incentives {
    * @returns {Promise<{ txHash: string } | { alreadyClaimed: true }>} The transaction hash or already claimed status
    * @throws Will throw an error if the incentives data is not found or the claim fails
    */
-  async claimIncentives(address: string): Promise<{ txHash: string } | { alreadyClaimed: true }> {
+  async claimIncentives(
+    address: string,
+  ): Promise<{ txHash: string } | { alreadyClaimed: true }> {
     if (!this.signer) {
-      throw new Error('Signer is required in claimIncentives')
+      throw new Error('Signer is required in claimIncentives');
     }
 
     try {
-      const incentivesData = await this.getIncentivesByAddress(address)
+      const incentivesData = await this.getIncentivesByAddress(address);
 
       if (!incentivesData?.contract_address) {
-        throw new Error(`No incentives found for address ${address}`)
+        throw new Error(`No incentives found for address ${address}`);
       }
 
       const isContractDeployed = await isContractAvailable(
         incentivesData.contract_address,
         this.provider as Provider,
-      )
+      );
 
       if (!isContractDeployed) {
-        throw new Error(`Merkle Distributor contract is not available at address ${incentivesData.contract_address}`)
+        throw new Error(
+          `Merkle Distributor contract is not available at address ${incentivesData.contract_address}`,
+        );
       }
 
-      const claimed = await this.isClaimed(incentivesData.contract_address, incentivesData.index)
+      const claimed = await this.isClaimed(
+        incentivesData.contract_address,
+        incentivesData.index,
+      );
 
       if (claimed) {
-        return { alreadyClaimed: true }
+        return { alreadyClaimed: true };
       }
 
       const { txHash } = await claimIncentivesFromMerkleDistributor({
@@ -69,12 +102,12 @@ export class Incentives {
         operatorAddress: incentivesData.operator_address,
         amount: incentivesData.amount,
         merkleProof: incentivesData.merkle_proof,
-      })
+      });
 
-      return { txHash }
+      return { txHash };
     } catch (error: any) {
-      console.log('Error claiming incentives:', error)
-      throw new Error(`Failed to claim incentives: ${error.message}`)
+      console.log('Error claiming incentives:', error);
+      throw new Error(`Failed to claim incentives: ${error.message}`);
     }
   }
 
@@ -86,8 +119,16 @@ export class Incentives {
    * @returns {Promise<boolean>} true if incentives are already claime
    *
    */
-  async isClaimed(contractAddress: ETH_ADDRESS, index: number): Promise<boolean> {
-    return await isClaimedFromMerkleDistributor(this.chainId, contractAddress, index, this.provider)
+  async isClaimed(
+    contractAddress: ETH_ADDRESS,
+    index: number,
+  ): Promise<boolean> {
+    return await isClaimedFromMerkleDistributor(
+      this.chainId,
+      contractAddress,
+      index,
+      this.provider,
+    );
   }
 
   /**
@@ -96,13 +137,13 @@ export class Incentives {
    * @throws On not found if address not found.
    */
   async getIncentivesByAddress(address: string): Promise<IncentivesType> {
-    const network = FORK_NAMES[this.chainId]
+    const network = FORK_NAMES[this.chainId];
     const incentives: IncentivesType = await this.request(
       `/${DEFAULT_BASE_VERSION}/address/incentives/${network}/${address}`,
       {
         method: 'GET',
       },
-    )
-    return incentives
+    );
+    return incentives;
   }
 }
